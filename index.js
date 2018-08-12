bf2f55838cf56470a5ef97d9e69e8976153e3f1e
deeasuquo@gmail.com <deeasuquo@gmail.com>
2018-08-12 16:16:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-// write your code below!
-var name = "Joe"
-var height = 74
+var name = "Susan"
+var Height = 74
 var message = ` is ${height} inches tall`
+console.log("Name:")
+console.log(name)
 
+console.log("Height:")
+console.log(height<40)
 module.exports = {name, height, message}
